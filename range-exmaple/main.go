971736c3000abb4ac73f25b745c1dc310762a245
Rudra Sarkar <rudrasarkar815@gmail.com>
2023-01-02 01:17:45 +0600

    range example

--- range-exmaple/main.go
+++ range-exmaple/main.go
@@ -11,4 +11,11 @@ func main() {
 	for index, fruit := range fruit_lists {
 		fmt.Printf("Index ID: %d, Fruit Name: %s\n", index, fruit)
 	}
+
+	// creating array exmple with no index number in range
+	num_list := []int{1, 2, 3, 4, 5, 6, 7}
+
+	for _, num := range num_list {
+		fmt.Printf("%d", num)
+	}
 }
