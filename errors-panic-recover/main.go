0c9118fa3c15a46b1907fb439cd9a1ae56263cdd
Rudra Sarkar <rudrasarkar815@gmail.com>
2023-01-03 17:59:28 +0600

    error example

---
+++ errors-panic-recover/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	// In Go, errors are represented using the error type, which is an interface type defined as follows:
+
+	/*
+		type error interface {
+			Error() string
+		}
+	*/
+
+	// error example with file operatinon
+	file_name, err := os.Open("hello.txt")
+
+	// error handling
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// used defer to close the file at the end
+	defer file_name.Close()
+
+	fmt.Println(file_name.Name()) // print file name just for example
+
+}
