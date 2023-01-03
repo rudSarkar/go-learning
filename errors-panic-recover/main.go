9eba5d9b7801e42de816551e3f82621d9af9523e
Rudra Sarkar <rudrasarkar815@gmail.com>
2023-01-03 18:25:11 +0600

    recover example

--- errors-panic-recover/main.go
+++ errors-panic-recover/main.go
@@ -40,9 +40,34 @@ func main() {
 	result := panic_div(10, 0)
 	fmt.Println(result)
 
+	result2 := recover_div(10, 0)
+	fmt.Println(result2) // this line will not be reached
+
+	/*
+		In this example, the div function divides x by y. If y is zero, it panics with a message saying "division by zero". If y is not zero, it returns the result of the division.
+
+		The defer statement at the beginning of the div function calls a function that calls recover. If a panic occurs within the div function, the recover function will catch it and print a message saying "Recovered from panic: division by zero".
+
+		If you run this code, it will print "Recovered from panic: division by zero" and continue execution. The line fmt.Println(result) will not be reached, because the div function returns before that line is executed.
+
+		Reference: recover_div()
+	*/
+
 }
 
 func panic_div(x, y int) int {
+	if y == 0 {
+		panic("division by zero")
+	}
+	return x / y
+}
+
+func recover_div(x, y int) int {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered from panic:", r)
+		}
+	}()
 	if y == 0 {
 		panic("division by zero")
 	}
